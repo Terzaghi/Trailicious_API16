26e8986154e857ca6fbb3ae3138922c15cd06f8c
Terzaghi <johnnie.17@gmail.com>
2015-01-15 16:10:21 +0100

    Custom Drawer (ListView con imagen)

--- app/src/main/java/jony/trailicious_api16/Adapters/NavigationDrawerAdapter.java
+++ app/src/main/java/jony/trailicious_api16/Adapters/NavigationDrawerAdapter.java
@@ -1,62 +1,163 @@
 package jony.trailicious_api16.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import jony.trailicious_api16.Models.ImageLoader;
 import jony.trailicious_api16.R;
 
-public class NavigationDrawerAdapter extends BaseAdapter {
-    private Context context;
-    String filasDrawer[];
-    int[] images = {R.drawable.icon_launcher, R.drawable.icon_launcher, R.drawable.icon_launcher, R.drawable.icon_launcher, R.drawable.icon_launcher};
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.ViewHolder> {
+    private static final int TYPE_HEADER = 0;  // Declaring Variable to Understand which View is being worked on
+    // IF the view under inflation and population is header or Item
+    private static final int TYPE_ITEM = 1;
 
-    public NavigationDrawerAdapter(Context context) {
+    private String mNavTitles[]; // String Array to store the passed titles Value from MainActivity.java
+    private int mIcons[];       // Int Array to store the passed icons resource value from MainActivity.java
+
+    private String name;        //String Resource for header View Name
+    //private int profile;        //int Resource for header view profileImage picture
+
+    String urlImagenPerfil;
+    Context context;
+
+    private Bitmap bitmap;
+    private String email;       //String Resource for header view email
+
+
+    // Creating a ViewHolder which extends the RecyclerView View Holder
+    // ViewHolder are used to to store the inflated views in order to recycle them
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        int Holderid;
+
+        TextView textView;
+        ImageView imageView;
+        ImageView profileImage;
+        TextView Name;
+        TextView email;
+
+
+        public ViewHolder(View itemView,int ViewType) {                 // Creating ViewHolder Constructor with View and viewType As a parameter
+            super(itemView);
+
+            // Here we set the appropriate view in accordance with the the view type as passed when the holder object is created
+
+            if(ViewType == TYPE_ITEM) {
+                textView = (TextView) itemView.findViewById(R.id.rowText); // Creating TextView object with the id of textView from item_row.xml
+                imageView = (ImageView) itemView.findViewById(R.id.rowIcon);// Creating ImageView object with the id of ImageView from item_row.xml
+                Holderid = 1;                                               // setting holder id as 1 as the object being populated are of type item row
+            }
+            else{
+
+
+                Name = (TextView) itemView.findViewById(R.id.name);         // Creating Text View object from header.xml for name
+                email = (TextView) itemView.findViewById(R.id.email);       // Creating Text View object from header.xml for email
+                profileImage = (ImageView) itemView.findViewById(R.id.circleView);// Creating Image view object from header.xml for profileImage pic
+                Holderid = 0;                                                // Setting holder id = 0 as the object being populated are of type header view
+            }
+        }
+
+
+    }
+
+
+
+    public NavigationDrawerAdapter(String Titles[],int Icons[],String Name,String Email, String url, Context context){ // MyAdapter Constructor with titles and icons parameter
+        // titles, icons, name, email, profile pic are passed from the main activity as we
+        mNavTitles = Titles;                //have seen earlier
+        mIcons = Icons;
+        name = Name;
+        email = Email;
+        //profile = Profile;                     //Imagen de perfil en modo int, Google+ nos da una url asi que lo hacemos con urlImagenPerfil
+        urlImagenPerfil = url;
         this.context = context;
-        filasDrawer = context.getResources().getStringArray(R.array.navigation_drawer_items);
-    }
-    @Override
-    public int getCount() {
-        //Número de elementos del ListView
-        return filasDrawer.length;
+
     }
 
-    @Override
-    public Object getItem(int position) {
-        //Elemento en la posición dada
-        return filasDrawer[position];
-    }
+
+
+    //Below first we ovverride the method onCreateViewHolder which is called when the ViewHolder is
+    //Created, In this method we inflate the item_row.xml layout if the viewType is Type_ITEM or else we inflate header.xml
+    // if the viewType is TYPE_HEADER
+    // and pass it to the view holder
 
     @Override
-    public long getItemId(int position) {
-        //No lo necesitamos pero debemos implementarlo obligatoriamente
-        return position;
-    }
+    public NavigationDrawerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+        if (viewType == TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.drawer_row,parent,false); //Inflating the layout
 
-        View row = null;
+            ViewHolder vhItem = new ViewHolder(v,viewType); //Creating ViewHolder and passing the object of type view
 
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = inflater.inflate(R.layout.custom_row, parent, false);
+            return vhItem; // Returning the created object
+
+            //inflate your layout and pass it to view holder
+
+        } else if (viewType == TYPE_HEADER) {
+
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.drawer_header,parent,false); //Inflating the layout
+
+            ViewHolder vhHeader = new ViewHolder(v,viewType); //Creating ViewHolder and passing the object of type view
+
+            return vhHeader; //returning the object created
+
+
         }
-        else {
-            row = convertView;
-        }
+        return null;
 
-        //Creamos cada row del ListView
-        TextView titleTextView1 = (TextView) row.findViewById(R.id.textView1);
-        ImageView titleImageView = (ImageView) row.findViewById(R.id.imageView1);
-
-        titleTextView1.setText(filasDrawer[position]);
-        titleImageView.setImageResource(images[position]);
-
-        return row;
     }
+
+    //Next we override a method which is called when the item in a row is needed to be displayed, here the int position
+    // Tells us item at which position is being constructed to be displayed and the holder id of the holder object tell us
+    // which view type is being created 1 for item row
+    @Override
+    public void onBindViewHolder(NavigationDrawerAdapter.ViewHolder holder, int position) {
+        if(holder.Holderid ==1) {                              // as the list view is going to be called after the header view so we decrement the
+            // position by 1 and pass it to the holder while setting the text and image
+            holder.textView.setText(mNavTitles[position - 1]); // Setting the Text with the array of our Titles
+            holder.imageView.setImageResource(mIcons[position -1]);// Settimg the image with array of our icons
+        }
+        else{
+
+            //TODO: intengo cargar la imagen de perfil desde una url
+            // holder.profileImage.setImageResource(profile);           // Similarly we set the resources for header view
+            int loader = R.drawable.icon_launcher; //Imagne mostrada mientras se carga de la URL
+            ImageLoader imgLoader = new ImageLoader(context);
+
+            imgLoader.DisplayImage(urlImagenPerfil, loader, holder.profileImage);
+
+            //TODO: FIN intengo cargar la imagen de perfil desde una url
+
+            holder.Name.setText(name);
+            holder.email.setText(email);
+        }
+    }
+
+    // This method returns the number of items present in the list
+    @Override
+    public int getItemCount() {
+        return mNavTitles.length+1; // the number of items in the list will be +1 the titles including the header view.
+    }
+
+
+    // Witht the following method we check what type of view is being passed
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
+
 }
--- app/src/main/java/jony/trailicious_api16/BaseActivity.java
+++ app/src/main/java/jony/trailicious_api16/BaseActivity.java
@@ -8,13 +8,16 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.plus.model.people.Person;
 
@@ -32,14 +35,140 @@ public class BaseActivity extends ActionBarActivity {
 
     Person persona; //Persona Logeada en la App
 
-    //Instancio el Adaptador de prueba:
-    private NavigationDrawerAdapter drawerAdapter;
+
+    // TODO Pruebas Drawer RecyclerView
+    //First We Declare Titles And Icons For Our Navigation Drawer List View
+    //This Icons And Titles Are holded in an Array as you can see
+
+    //String TITLES[] = getApplicationContext().getResources().getStringArray(R.array.navigation_drawer_items);// {"Home","Events","Mail","Shop","Travel"};
+    String TITLES[] = {"Home","Events","Mail","Shop","Travel"};
+
+    int ICONS[] = {R.drawable.icon_launcher,R.drawable.icon_launcher,R.drawable.icon_launcher,R.drawable.icon_launcher,R.drawable.icon_launcher};
+
+    //Similarly we Create a String Resource for the name and email in the drawer_header view
+    //And we also create a int resource for profile picture in the drawer_header view
+
+    String NAME = "Akash Bangad";
+    String EMAIL = "akash.bangad@android4devs.com";
+    //int PROFILE = R.drawable.icon_launcher;
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;                  // Declaring Action Bar Drawer Toggle
+
+    // TODO FIN Pruebas Drawer RecyclerView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
+        // TODO Pruebas Drawer RecyclerView
+        persona = (Person) getIntent().getParcelableExtra("current_person") ;
+        NAME = persona.getDisplayName();
+        //EMAIL = persona.getEmails().get(0).getValue();
+
+        /*TODO: Obtengo la imagen de perfil*/
+        //PROFILE = persona.getImage().hashCode(); //TODO: No funciona
+
+        //URL de la imagen:
+        String url = persona.getImage().getUrl();
+
+
+        /*TODO: FIN Obtengo la imagen de perfil*/
+
+
+        /* Assinging the toolbar object ot the view
+        and setting the the Action bar to our toolbar
+         */
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setLogo(R.drawable.logo_blanco);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mAdapter = new NavigationDrawerAdapter(TITLES,ICONS,NAME,EMAIL,url, getApplicationContext());       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+
+        //Evento Click del Drawer
+        final GestureDetector mGestureDetector = new GestureDetector(BaseActivity.this, new GestureDetector.SimpleOnGestureListener() {
+
+            @Override public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+        });
+
+
+        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+                View child = recyclerView.findChildViewUnder(motionEvent.getX(),motionEvent.getY());
+
+
+
+                if(child!=null && mGestureDetector.onTouchEvent(motionEvent)){
+                    Drawer.closeDrawers();
+                    Toast.makeText(BaseActivity.this, "The Item Clicked is: " + recyclerView.getChildPosition(child), Toast.LENGTH_SHORT).show();
+
+                    return true;
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+            }
+        });
+
+
+
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.drawer_layout);        // Drawer object Assigned to the view
+
+        // Sombra que se sobrepone al contenido principal cuando el drawer está abierto
+        Drawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        Drawer.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
+        // TODO FIN Pruebas Drawer RecyclerView
+/*
         //Obtengo los datos de la persona logeada
         persona = (Person) getIntent().getParcelableExtra("current_person") ;
 
@@ -62,10 +191,11 @@ public class BaseActivity extends ActionBarActivity {
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayoutt, toolbar, R.string.app_name, R.string.app_name);
         drawerLayoutt.setDrawerListener(actionBarDrawerToggle);
-
+*/
         // enable ActionBar app icon to behave as action to toggle nav drawer
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
 
 
         if (savedInstanceState == null) {
@@ -97,12 +227,13 @@ public class BaseActivity extends ActionBarActivity {
     }
 
     /* The click listner for ListView in the navigation drawer */
+    /* TODO: evento click del Antiguo ListView Drawer
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             selectItem(position);
         }
-    }
+    }*/
 
     /**
      * Selección de un item en la lista del navigation drawer
@@ -133,20 +264,20 @@ public class BaseActivity extends ActionBarActivity {
         fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
 
         //Selecciono el elemento en el Navigation Drawer y le cierro
-        listView.setItemChecked(position, true);
-        drawerLayoutt.closeDrawer(listView);
+        //TODO Drawer ya no es un ListView listView.setItemChecked(position, true);
+//TODO error con nuevo drawer        drawerLayoutt.closeDrawer(listView);
     }
 
     /**
      * When using the ActionBarDrawerToggle, you must call it during
      * onPostCreate() and onConfigurationChanged()...
      */
-
+/*
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
-        actionBarDrawerToggle.syncState();
+//TODO error con nuevo drawer        actionBarDrawerToggle.syncState();
     }
 
     @Override
@@ -155,6 +286,8 @@ public class BaseActivity extends ActionBarActivity {
         // Pass any configuration change to the drawer toggls
         actionBarDrawerToggle.onConfigurationChanged(newConfig);
     }
+
+   */
 
 }
 
